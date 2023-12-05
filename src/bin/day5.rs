d65f9106700da79f7a32ca35aa3d9924b0d075d2
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2023-12-06 00:26:44 +1100

    Interesting WIP on a better day 5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,6 +1,6 @@
+use advent_of_code2023::range::Range;
 use color_eyre::Result;
 use itertools::Itertools;
-use num::range;
 use std::time::Instant;
 
 #[derive(Debug, Clone)]
@@ -123,12 +123,96 @@ fn solve_part2(input: &Input) -> u64 {
         .iter()
         .tuples()
         .flat_map(|(seed_start, seed_length)| {
-            range(*seed_start, *seed_start + *seed_length - 1)
+            num::range(*seed_start, *seed_start + *seed_length - 1)
                 .into_iter()
                 .map(|seed| get_location_for_seed(seed, &input.mappings))
         })
         .min()
         .unwrap()
+}
+
+#[derive(Debug, Clone, Copy)]
+struct MappingRange {
+    source: Range<u64>,
+    destination: Range<u64>,
+}
+
+impl MappingRange {
+    pub fn unmapped(start: u64, end: u64) -> MappingRange {
+        MappingRange {
+            source: (start, end).into(),
+            destination: (start, end).into(),
+        }
+    }
+}
+
+impl From<&MappingRow> for MappingRange {
+    fn from(value: &MappingRow) -> Self {
+        MappingRange {
+            source: (value.source_start, value.source_start + value.length - 1).into(),
+            destination: (
+                value.destination_start,
+                value.destination_start + value.length - 1,
+            )
+                .into(),
+        }
+    }
+}
+
+fn build_mapping_ranges(
+    mapping_ranges: &Vec<MappingRange>,
+    mappings: &Vec<MappingRow>,
+) -> Vec<MappingRange> {
+    // Combine a series of ranges with another set of mappings
+    // Effectively composing the operations
+    // Collapse the destination of ranges into the destination of ranges + mappings
+    vec![]
+}
+
+// Alternate concept: calculate every mapping in terms of ranges.
+// seed-to-soil intervals then break down into sub-intervals for soil-to-fertilizer
+// Max number of intervals is still << number of seeds
+fn solve_part2_alt(input: &Input) -> u64 {
+    let upper_bound = input
+        .seeds
+        .iter()
+        .tuples()
+        .map(|(start, length)| start + length - 1)
+        .max()
+        .unwrap();
+    // Create a list of non-overlapping but contiguous ranges
+    let covered_ranges = input.mappings[0]
+        .iter()
+        .map(|map| MappingRange::from(map))
+        .sorted_by_key(|r| r.source.lower)
+        .collect_vec();
+    let mut all_ranges = covered_ranges
+        .iter()
+        .tuple_windows()
+        .flat_map(|(r1, r2)| {
+            if r1.source.upper < r2.source.lower - 1 {
+                // Fill the gap
+                vec![
+                    *r1,
+                    MappingRange::unmapped(r1.source.upper, r2.source.lower),
+                    *r2,
+                ]
+            } else {
+                vec![*r1, *r2]
+            }
+        })
+        .collect_vec();
+    if all_ranges[0].source.lower > 0 {
+        all_ranges.insert(0, MappingRange::unmapped(0, all_ranges[0].source.lower - 1));
+    }
+    if all_ranges.last().unwrap().source.upper < upper_bound {
+        all_ranges.push(MappingRange::unmapped(
+            all_ranges.last().unwrap().source.upper,
+            upper_bound,
+        ));
+    }
+    println!("{:?}", all_ranges);
+    1
 }
 
 #[cfg(test)]
@@ -147,6 +231,14 @@ mod tests {
     fn test_part2() -> color_eyre::Result<()> {
         let input = parsing::parse_input(include_str!("../../input/day5.test.txt"))?;
         let result = solve_part2(&input);
+        assert_eq!(result, 46);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2_alt() -> color_eyre::Result<()> {
+        let input = parsing::parse_input(include_str!("../../input/day5.test.txt"))?;
+        let result = solve_part2_alt(&input);
         assert_eq!(result, 46);
         Ok(())
     }
--- src/range.rs
+++ src/range.rs
@@ -1,41 +1,48 @@
 use std::cmp::{max, min};
 
+use num::PrimInt;
 use reformation::Reformation;
 
 #[derive(Reformation, Clone, Copy, Debug, Hash, PartialEq, Eq)]
 #[reformation("{lower}-{upper}")]
-pub struct Range {
-    pub lower: i32,
-    pub upper: i32,
+pub struct Range<T: PrimInt> {
+    pub lower: T,
+    pub upper: T,
 }
 
-impl From<(i32, i32)> for Range {
+impl From<(i32, i32)> for Range<i32> {
     fn from((lower, upper): (i32, i32)) -> Self {
         Range { lower, upper }
     }
 }
 
-impl Range {
-    pub fn is_subrange_inclusive(&self, other: &Range) -> bool {
+impl From<(u64, u64)> for Range<u64> {
+    fn from((lower, upper): (u64, u64)) -> Self {
+        Range { lower, upper }
+    }
+}
+
+impl <T: PrimInt> Range<T> {
+    pub fn is_subrange_inclusive(&self, other: &Range<T>) -> bool {
         self.lower >= other.lower && self.upper <= other.upper
     }
-    pub fn is_subrange_exclusive(&self, other: &Range) -> bool {
+    pub fn is_subrange_exclusive(&self, other: &Range<T>) -> bool {
         self.lower > other.lower && self.upper < other.upper
     }
 
-    pub fn overlap(&self, other: &Range) -> bool {
+    pub fn overlap(&self, other: &Range<T>) -> bool {
         (self.lower >= other.lower && self.lower <= other.upper)
             || (self.upper <= other.upper && self.upper >= other.lower)
             || self.is_subrange_inclusive(other)
             || other.is_subrange_inclusive(self)
     }
 
-    pub fn overlap_or_adjacent(&self, other: &Range) -> bool {
-        self.overlap(other) || self.upper == other.lower - 1 || other.upper == self.lower - 1
+    pub fn overlap_or_adjacent(&self, other: &Range<T>) -> bool {
+        self.overlap(other) || self.upper == other.lower - T::one() || other.upper == self.lower - T::one()
     }
 
     // assume overlap
-    pub fn merge(&self, other: &Range) -> Range {
+    pub fn merge(&self, other: &Range<T>) -> Range<T> {
         Range {
             lower: min(self.lower, other.lower),
             upper: max(self.upper, other.upper),
